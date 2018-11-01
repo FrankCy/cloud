9a3ab08390d127555afbf685de03a7b142e1cca2
cyoung <cy880708@163.com>
2018-11-01 20:11:49 +0800

    提交Config配置

---
+++ cloud-common/src/main/java/com/spring/cloud/common/CloudCommonApplication.java
@@ -0,0 +1,12 @@
+package com.spring.cloud.common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudCommonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudCommonApplication.class, args);
+	}
+}
