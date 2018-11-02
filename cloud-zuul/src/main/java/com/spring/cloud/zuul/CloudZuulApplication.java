d59b24fa7cb0e35a898aaec64b2d68c38ddfebe3
cyoung <cy880708@163.com>
2018-11-02 15:38:04 +0800

    提交

--- cloud-zuul/src/main/java/com/spring/cloud/zuul/CloudZuulApplication.java
+++ cloud-zuul/src/main/java/com/spring/cloud/zuul/CloudZuulApplication.java
@@ -2,8 +2,14 @@ package com.spring.cloud.zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableZuulProxy
+@EnableCircuitBreaker
 public class CloudZuulApplication {
 
     public static void main(String[] args) {
